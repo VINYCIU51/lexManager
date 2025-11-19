1043dc6e22c401e9f3576e2bc02c21c1add04600
Vinycius <vinyciusalves155@gmail.com>
2025-11-18 22:57:49 -0300

    chore: adicionados arquivos requirements e gitignore

--- App/templates/models.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-#função para abrir conexão com o banco de dados.
-def conectar():
-    conexao = sqlite3.connect('bd_advocacia.db')
-    return conexao
-
-conexao = conectar()
-conexao.close()
